70dc08755f95ee5d2d84fe153c8bd0335bf070d3
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-07-01 16:52:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "environment" {}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  default = "10.0.4.0/24"
+}
+
+variable "db_username" {
+  default = "postgres"
+}
+
+variable "db_password" {
+  sensitive = true
+}
