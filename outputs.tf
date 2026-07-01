a9fe5361128ed7a345fc44f84b6b18a742b63ca2
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-07-01 16:52:54 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "ecs_cluster" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "service" {
+  value = aws_ecs_service.app.name
+}
+
+output "alb_dns" {
+  value = aws_lb.app.dns_name
+}
+
+output "db_endpoint" {
+  value     = aws_db_instance.postgres.endpoint
+  sensitive = true
+}
+``
