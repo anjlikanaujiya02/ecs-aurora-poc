6ef809f1b8d6e7379bb7994a7799688dcdb458f5
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-07-02 15:09:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,16 @@
+output "alb_dns" {
+  value = aws_lb.app.dns_name
+}
+
 output "ecs_cluster" {
   value = aws_ecs_cluster.main.name
 }
 
-output "service" {
+output "ecs_service" {
   value = aws_ecs_service.app.name
-}
-
-output "alb_dns" {
-  value = aws_lb.app.dns_name
 }
 
 output "db_endpoint" {
   value     = aws_db_instance.postgres.endpoint
   sensitive = true
 }
-
